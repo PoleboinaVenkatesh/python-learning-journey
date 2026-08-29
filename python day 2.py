ae16a264ad6a7e61ece9cef013f354dad15c2bc3
PoleboinaVenkatesh <poleboinavenkatesh07@gmail.com>
2026-08-29 23:08:08 +0530

    Add files via upload

---
+++ python day 2.py
@@ -0,0 +1,173 @@
+
+
+a=23
+# gdhsjugyfhsajlfugsrkfhsufsflikfgkfsuyifsu
+#cfvgbhnjklkjhjghfdfghjjgfdfghjdcvgftgngggggftr
+#hvghjygyxjhdoygligodghsdihgggdlghiygdkhipu[piopj
+#hfldglidghbgdihgbdgiddjndgdbndkdkjgbl
+
+
+
+2+223344
+
+
+
+3456-543
+
+
+
+
+5437478*32343
+
+
+
+
+
+6567/34
+
+
+
+
+
+657//6
+
+
+
+
+53%6
+
+
+
+
+3**9
+#** is for exponential operation
+
+
+
+
+
+9>3
+
+
+
+7>=7
+
+
+
+8<10
+
+
+
+8>=15
+
+
+
+True and True and True and False
+
+
+
+True or True or True or True or True or True
+
+
+
+False and False and False or 0
+
+
+
+
+5<13 and 81<93 and 31>23 and  10==10
+
+
+
+
+4>2 and 97<87 and 52>45 and 10==10
+
+
+
+
+"fsdsgfjsrg" and "jggdjg"
+
+
+
+
+a=a+1
+
+
+
+a+=1
+
+
+
+a=a-1
+
+
+
+a-=1
+
+
+
+a=1
+b=1
+a==b
+
+
+
+a is b
+
+
+
+a is int
+a is bool
+a is str
+a is float
+
+
+
+
+type(a) is int()
+
+
+44 in[44,536, 43,5,32,54,23]
+
+
+
+5654 in [545,76236,7535,767,626]
+
+
+43 not in [54,7567,98,998,99]
+
+
+
+a=41
+b=41
+if a==b:
+    print("a is equal to b")
+
+
+a=41
+b=41
+if a==b:
+  print("a is equal to b")
+print("out of if")
+
+
+
+a=41
+b=41
+if a==b:
+  print("a is equal to b")
+  print("out of if")
+
+
+
+a=1
+b=9
+if a==b:
+  print("a is equal to b")
+print("out of if")
+
+
+
+
+
+
