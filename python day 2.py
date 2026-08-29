bfbbe32169bff00637215d71b95eeac6bccb183c
PoleboinaVenkatesh <poleboinavenkatesh07@gmail.com>
2026-08-29 23:21:13 +0530

    Add files via upload

--- python day 2.py
+++ python day 2.py
@@ -1,173 +1,129 @@
+# Variable Assignment
 
+a = 23
 
-a=23
-# gdhsjugyfhsajlfugsrkfhsufsflikfgkfsuyifsu
-#cfvgbhnjklkjhjghfdfghjjgfdfghjdcvgftgngggggftr
-#hvghjygyxjhdoygligodghsdihgggdlghiygdkhipu[piopj
-#hfldglidghbgdihgbdgiddjndgdbndkdkjgbl
+# Arithmetic Operators
 
+2 + 223344
 
+3456 - 543
 
-2+223344
+5437478 * 32343
 
+6567 / 34
 
+657 // 6
 
-3456-543
+53 % 6
 
+# Exponential Operator
 
+3 ** 9
+# ** is for exponential operation
 
+# Comparison Operators
 
-5437478*32343
+9 > 3
 
+7 >= 7
 
+8 < 10
 
+8 >= 15
 
-
-6567/34
-
-
-
-
-
-657//6
-
-
-
-
-53%6
-
-
-
-
-3**9
-#** is for exponential operation
-
-
-
-
-
-9>3
-
-
-
-7>=7
-
-
-
-8<10
-
-
-
-8>=15
-
-
+# Logical Operators
 
 True and True and True and False
 
-
-
 True or True or True or True or True or True
-
-
 
 False and False and False or 0
 
+# Multiple Conditions Using and
 
+5 < 13 and 81 < 93 and 31 > 23 and 10 == 10
 
+4 > 2 and 97 < 87 and 52 > 45 and 10 == 10
 
-5<13 and 81<93 and 31>23 and  10==10
-
-
-
-
-4>2 and 97<87 and 52>45 and 10==10
-
-
-
+# String with Logical Operators
 
 "fsdsgfjsrg" and "jggdjg"
 
+# Assignment Operators
 
+a = a + 1
 
+a += 1
 
-a=a+1
+a = a - 1
 
+a -= 1
 
+# Equality Operator
 
-a+=1
+a = 1
+b = 1
 
+a == b
 
-
-a=a-1
-
-
-
-a-=1
-
-
-
-a=1
-b=1
-a==b
-
-
+# Identity Operator
 
 a is b
 
-
+# Identity Comparison with Data Types
 
 a is int
 a is bool
 a is str
 a is float
 
-
-
+# Type Checking
 
 type(a) is int()
 
+# Membership Operator
 
-44 in[44,536, 43,5,32,54,23]
+44 in [44, 536, 43, 5, 32, 54, 23]
 
+5654 in [545, 76236, 7535, 767, 626]
 
+# Not in Operator
 
-5654 in [545,76236,7535,767,626]
+43 not in [54, 7567, 98, 998, 99]
 
+# if Statement
 
-43 not in [54,7567,98,998,99]
+a = 41
+b = 41
 
-
-
-a=41
-b=41
-if a==b:
+if a == b:
     print("a is equal to b")
 
+# if Statement with Code Outside the Block
 
-a=41
-b=41
-if a==b:
-  print("a is equal to b")
+a = 41
+b = 41
+
+if a == b:
+    print("a is equal to b")
+
 print("out of if")
 
+# Multiple Statements Inside if
 
+a = 41
+b = 41
 
-a=41
-b=41
-if a==b:
-  print("a is equal to b")
-  print("out of if")
+if a == b:
+    print("a is equal to b")
+    print("out of if")
 
+# if Statement with False Condition
 
+a = 1
+b = 9
 
-a=1
-b=9
-if a==b:
-  print("a is equal to b")
+if a == b:
+    print("a is equal to b")
+
 print("out of if")
-
-
-
-
-
-
